dc5f65b1707a8a47970e97717c36dce49a8729c5
yzz <1314928757@163.com>
2020-12-31 19:44:32 +0800

    12.31

--- practice_mybatis-plus/s1.sql
+++ practice_mybatis-plus/s1.sql
@@ -1,5 +1,11 @@
 set global wait_timeout=604800;
 
 
-
 show global variables like 'wait_timeout';
+
+
+show status like 'Threads%';
+
+SHOW VARIABLES LIKE '%max_connections%';  
+
+select * from stu;
