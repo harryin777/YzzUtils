28fcc587ee93aa0ce5f2b7b3f47d413ccc94881b
harryin777 <13149287572@163.com>
2021-04-07 17:54:03 +0800

    布隆过滤器

---
+++ worthlessamodule/src/main/java/com/yzz/wam/filter/BloomFilter.java
@@ -0,0 +1,12 @@
+package com.yzz.wam.filter;
+
+import java.io.Serializable;
+
+/**
+ * @ClassName BloomFilter
+ * @Author yky
+ * @Date 2021/4/7
+ * @Version 1.0MisjudgmentRate
+ */
+public class BloomFilter implements Serializable {
+}
