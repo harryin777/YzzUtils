7e3637b0ced6351bd25b3e88e3f3dba935e509c4
yzz <1314928757@163.com>
2021-04-13 23:12:16 +0800

    CodeGenerator

--- mybatis_plus/src/test/java/com/yzz/practice_mybatisplus/CodeGenerator.java
+++ mybatis_plus/src/test/java/com/yzz/practice_mybatisplus/CodeGenerator.java
@@ -2,6 +2,13 @@ package com.yzz.practice_mybatisplus;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import org.junit.Test;
 
 /**
