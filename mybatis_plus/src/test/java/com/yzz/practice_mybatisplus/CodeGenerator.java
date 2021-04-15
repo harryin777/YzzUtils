e4df0c32cd7c83f55b4d729f1f25cf6d6fd15c35
harryin777 <13149287572@163.com>
2021-04-15 17:52:40 +0800

    CodeGenerator

--- mybatis_plus/src/test/java/com/yzz/practice_mybatisplus/CodeGenerator.java
+++ mybatis_plus/src/test/java/com/yzz/practice_mybatisplus/CodeGenerator.java
@@ -16,6 +16,7 @@ import org.junit.Test;
  * @Author yky
  * @Date 2020/12/25
  * @Version 1.0
+ * 需要增加依赖 按照官网的
  */
 public class CodeGenerator {
 	
@@ -50,7 +51,7 @@ public class CodeGenerator {
 		
 		// 4、包配置
 		PackageConfig pc = new PackageConfig();
-		pc.setModuleName("edu"); //模块名
+		pc.setModuleName("mybatis_plus"); //模块名
 		pc.setParent("com.picc.hirc.management.blacklist");
 		pc.setController("controller");
 		pc.setEntity("entity");
@@ -60,7 +61,7 @@ public class CodeGenerator {
 		
 		// 5、策略配置
 		StrategyConfig strategy = new StrategyConfig();
-		strategy.setInclude("blacklistcontrol", "blacklistcontrol_salve");
+		strategy.setInclude("black_list");
 		strategy.setNaming(NamingStrategy.underline_to_camel);//数据库表映射到实体的命名策略
 		strategy.setTablePrefix(pc.getModuleName() + "_"); //生成实体时去掉表前缀
 		
